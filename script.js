81c932213abae4f7ec6693c4f77b454b091f02fe
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-07 16:27:01 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// JavaScript
+function expandPanel(panelNumber) {
+  // Reset all panels to default size
+  for (let i = 1; i <= 5; i++) {
+    const panel = document.getElementById(`panel-${i}`);
+    panel.classList.remove("active");
+  }
+
+  // Expand the clicked panel
+  const clickedPanel = document.getElementById(`panel-${panelNumber}`);
+  clickedPanel.classList.add("active");
+}
